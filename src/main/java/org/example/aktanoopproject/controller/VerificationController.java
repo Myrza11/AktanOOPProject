a2562bd63bc00c6d00186e088b4407381838666f
Myrza <bapaevmyrza.work@example.com>
2025-05-21 11:51:38 +0600

    project is finished

---
+++ src/main/java/org/example/aktanoopproject/controller/VerificationController.java
@@ -0,0 +1,28 @@
+package org.example.aktanoopproject.controller;
+
+import org.example.aktanoopproject.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/verify")
+
+public class VerificationController {
+
+    @Autowired
+    private EmailService emailService;
+
+
+    @GetMapping
+    public ResponseEntity<String> verifyEmail(@RequestParam String token) {
+        return ResponseEntity.ok("Email подтвержден, теперь вы можете войти    n/n   " + emailService.verifyEmail(token));
+
+    }
+
+    @GetMapping("/exists")
+    public boolean isEmailExist(@RequestParam String email) {
+        return emailService.isEmailExist(email);
+    }
+
+}
